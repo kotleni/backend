0a011d79884ea31dd47928a0aac70d6df0cff01e
Viktor Varenik <yavarenikya@gmail.com>
2025-12-06 22:55:06 +0200

    chore: yes we do trust proxy

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,16 @@
 import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors({
     origin: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
+  app.set('trust proxy', true);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
